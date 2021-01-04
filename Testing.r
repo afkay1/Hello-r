5e8a5ad8b506e58da8cc5c18ff9504aee0c554b0
afkay1 <khanfarhan590@gmail.com>
2021-01-04 16:16:38 +0500

    Lines added

--- Testing.r
+++ Testing.r
@@ -7,3 +7,12 @@ version = "Rstudio - in writing"
 print(name)
 
 print("these line has been written in Rstudio")
+
+laptop = "i need to buy new laptop for my works"
+
+ print(laptop)
+RSTO = ("the R programe is more difficult then paython")
+alnafi = "it is the best platform to learn programing language"
+cods = "these track are best for data sceince"
+
+print(RSTO)
